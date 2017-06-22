7ea239121f714804f0bad668cd46f28e71bfcfe6
jkeiffer1 <saros82@hotmail.com>
2017-06-22 16:29:17 -0400

    1 to 100, %3 = Mined, %5 = Minds, %both =MinedMinds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,7 +1,9 @@
 number = 1
 
 100.times do 
-	if (number % 3 == 0)
+	if (number % 3 == 0 && number % 5 == 0)
+		puts "MinedMinds"
+	elsif (number % 3 == 0)
 		puts "Mined"
 	elsif (number % 5 == 0)
 		puts "Minds"
