48454564f32f0aeb70bd451eafdbb026c8ccb9a6
jkeiffer1 <saros82@hotmail.com>
2017-06-22 16:25:03 -0400

    1-100 divisible by 3 equals Mined and divisible by 5 equals Minds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,9 +1,12 @@
 number = 1
 
 100.times do 
-	if (number % 5 == 0)
+	if (number % 3 == 0)
+		puts "Mined"
+	elsif (number % 5 == 0)
 		puts "Minds"
-	else puts number
+	else
+		puts number
 	end
 	number = number + 1
 end
