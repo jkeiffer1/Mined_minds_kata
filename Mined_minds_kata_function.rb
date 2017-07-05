f1157457115cca55f5f4f6c5cf16e0a7e366fe33
jkeiffer1 <saros82@hotmail.com>
2017-07-05 14:21:41 -0400

    Unit testing: Mined minds kata function

---
+++ Mined_minds_kata_function.rb
@@ -0,0 +1,7 @@
+def mm_kata(num)
+	if num == 3
+		"Mined"
+	else
+		num
+	end
+end
---
+++ test_Mined_minds_kata_function.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require_relative "Mined_minds_kata_function.rb"
+class Kata_test < Minitest::Test
+
+	def test_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_failure_1_noteql_1
+		assert_equal(1,2)
+	end
+
+	def test_modulo
+		assert_equal(3 % 3, 0)
+	end
+
+	def test_mined_minds_kata_1
+		assert_equal(1, mm_kata(1))
+	end
+
+	def test_return_2
+		assert_equal(2, mm_kata(2))
+	end
+
+	def test_return_3
+		assert_equal("Mined", mm_kata(3))
+	end
+end
