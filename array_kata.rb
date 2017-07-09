3dc91c1aa75031113b77cad6d2e6324fd1e847b2
jkeiffer1 <saros82@hotmail.com>
2017-07-08 20:31:53 -0400

    First unit test of array functionality successful

---
+++ array_kata.rb
@@ -0,0 +1,5 @@
+def array_kata(x)
+	a = (1..100).to_a 
+	a[x]
+end
+
--- test_array_kata.rb
+++ test_array_kata.rb
@@ -5,4 +5,8 @@ class Array_kata_test < Minitest::Test
 	def test_1_equals_1
 		assert_equal(1,1)
 	end
+
+	def test_array_functionality
+		assert_equal(4,array_kata(3))
+	end
 end
