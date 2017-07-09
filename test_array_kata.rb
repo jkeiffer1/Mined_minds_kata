3c9ce85bdb71f3c9e18945626795d14e38279aea
jkeiffer1 <saros82@hotmail.com>
2017-07-08 20:34:23 -0400

    array test 2 successful

--- test_array_kata.rb
+++ test_array_kata.rb
@@ -6,7 +6,11 @@ class Array_kata_test < Minitest::Test
 		assert_equal(1,1)
 	end
 
-	def test_array_functionality
+	def test_array_1
 		assert_equal(4,array_kata(3))
+	end
+
+	def test_array_2
+		assert_equal(98,array_kata(97))
 	end
 end
